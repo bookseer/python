8a6cbca460117a46356c670fd9a8bf94392ca913
askras <askrasnikov@gmail.com>
2023-07-10 17:29:14 +0300

    Fixed PEP8 problems for Collatz Conjecture project

---
+++ projects/collatz_conjecture/collatz_conjecture.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Collatz Conjecture or the (3n + 1) Problem
+"""
+
+import time
+
+from syracuse_sequence_iterative import get_syracuse_sequence_iterative as get_syracuse_sequence
+
+
+def main():
+    while True:
+        print("Enter a starting number greater than 0 or QUIT:")
+        response = input("> ") or "QUIT"
+
+        if response == "QUIT":
+            input("Press ENTER to exit")
+            break
+        elif not response.isdecimal() or response == "0":
+            print("You must enter an integer greater than 0.")
+            continue
+        else:
+            response_n = int(response)
+
+        print(f"For n = {response_n}, the Syracuse sequence has the form:")
+        syracuse_sequence = get_syracuse_sequence(response_n)
+        for item in syracuse_sequence[:-1]:
+            print(str(item) + ",", end=" ", flush=True)
+            time.sleep(0.1)
+        print(syracuse_sequence[-1], end="\n\n")
+
+
+if __name__ == "__main__":
+
+    annotation = """\
+To explain the essence of the hypothesis, consider the following
+sequence of numbers, called the Syracuse sequence.
+We take any natural number n.
+    1) If n is even, the next number n is n / 2.
+    2) If n is odd, the next number n is n * 3 + 1.
+    3) If n is 1, stop. Otherwise, repeat.
+
+It is generally thought, but so far not mathematically proven, that
+every starting number eventually terminates at 1.
+
+"""
+
+    print(annotation)
+    main()
---
+++ projects/collatz_conjecture/syracuse_sequence_recursive.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Syracuse Sequence. Recursive implementation.
+
+Project: TryPython
+A collection of educational materials for learning the Python
+
+Author: Alexander Krasnikov aka askras
+Website: trypython.pro
+
+License: BSD 3 clause
+"""
+
+def get_syracuse_sequence_recursive(n: int) -> list[int]:
+    """Syracuse_sequence. Recursive implementation.
+
+    To explain the essence of the hypothesis, consider the following sequence
+    of numbers, called the Syracuse sequence. We take any natural number n.
+    1) If n is even, the next number n is n / 2.
+    2) If n is odd, the next number n is n * 3 + 1.
+    3) If n is 1, stop. Otherwise, repeat.
+    It is generally thought, but so far not mathematically proven, that
+    every starting number eventually terminates at 1.
+
+    Parameters
+    ----------
+    n : int
+        The first element of the Syracuse sequence.
+
+    Returns
+    -------
+    syracuse_sequence : list[int]
+        Syracuse sequence with the first element 'n'
+
+    Notes
+    -----
+    Recursive implementation of the Syracuse sequence calculation.
+
+    References
+    ----------
+    [1] Collatz Sequence : https://en.wikipedia.org/wiki/Collatz_conjecture
+
+    Examples
+    --------
+    >>> get_syracuse_sequence_recursive(0)
+    []
+
+    >>> get_syracuse_sequence_recursive(1)
+    [1]
+
+    >>> get_syracuse_sequence_recursive(5)
+    [5, 16, 8, 4, 2, 1]
+
+    >>> get_syracuse_sequence_recursive(27) # doctest: +ELLIPSIS
+    [27, 82, 41, 124, 62, 31, 94, 47, 142, 71, 214, 107, 322, 161, ...
+
+    >>> get_syracuse_sequence_recursive(150)
+    [150, 75, 226, 113, 340, 170, 85, 256, 128, 64, 32, 16, 8, 4, 2, 1]
+    """
+    if n < 1:
+        return []
+    elif n == 1:  # n is even
+        return [1]
+    elif n % 2 == 0:  # n is odd
+        return [n] + get_syracuse_sequence_recursive(n // 2)
+    else:
+        return [n] + get_syracuse_sequence_recursive(3 * n + 1)
+
+
+if __name__ == "__main__":
+    print("Examples of the Syracuse sequence.", end="\n\n")
+
+    test_n = 0
+    print(f"For n = {test_n}, the Syracuse sequence has the form:")
+    print(get_syracuse_sequence_recursive(test_n), end="\n\n")
+
+    test_n = 1
+    print(f"For n = {test_n}, the Syracuse sequence has the form:")
+    print(get_syracuse_sequence_recursive(test_n), end="\n\n")
+
+    test_n = 5
+    print(f"For n = {test_n}, the Syracuse sequence has the form:")
+    print(get_syracuse_sequence_recursive(test_n), end="\n\n")
+
+    test_n = 27
+    print(f"For n = {test_n}, the Syracuse sequence has the form:")
+    print(get_syracuse_sequence_recursive(test_n), end="\n\n")
+
+    test_n = 150
+    print(f"For n = {test_n}, the Syracuse sequence has the form:")
+    print(get_syracuse_sequence_recursive(test_n), end="\n\n")
