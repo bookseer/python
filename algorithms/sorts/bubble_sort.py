3e7619bdf1b7eb0b6be916193ae144e09b7bfa62
askras <askrasnikov@gmail.com>
2023-07-21 20:47:29 +0300

    Added the `Bubble Sort` algorithm

---
+++ algorithms/sorts/bubble_sort.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Bubble Sort
+
+Project: TryPython
+A collection of educational materials for learning the Python
+
+Author: Alexander Krasnikov aka askras
+Website: trypython.pro
+
+License: BSD 3 clause
+"""
+
+import typing
+
+from collections.abc import Collection
+
+
+class Comparable(typing.Protocol):
+    def __lt__(self: 'TypeItems', other: 'TypeItems') -> bool:
+        ...
+
+
+# Type of collection items
+TypeItems = typing.TypeVar('TypeItems', bound=Comparable)
+
+
+def bubble_sort(iterable: Collection[TypeItems], /) -> list[TypeItems]:
+    """Pure implementation of bubble sort algorithm in Python
+
+    Parameters
+    ----------
+    iterable : Collection[TypeItems]
+        Some ordered collection with heterogeneous comparable items inside.
+
+    Returns
+    -------
+    list(TypeItems)
+         A new list containing all items from the `iterable` in ascending order.
+
+    References
+    ----------
+    [1] Bubble Sort : https://en.wikipedia.org/wiki/Bubble_sort
+
+    Examples
+    --------
+    >>> bubble_sort([4, 3, 8, 2, 7, 1, 9, 5, 0, 6])
+    [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    >>> bubble_sort([4, 2, 3, 1, 5]) == sorted([4, 2, 3, 1, 5])
+    True
+    >>> bubble_sort([]) == sorted([])
+    True
+    >>> bubble_sort([4, 7, 2, 11, 1, 7, 0]) == sorted([4, 7, 2, 11, 1, 7, 0])
+    True
+    >>> bubble_sort([-2, 10, 16, -3, -11]) == sorted([-2, 10, 16, -3, -11])
+    True
+    >>> letter_collection = ['a', 'c', 'e', 'b', 'd']
+    >>> bubble_sort(letter_collection) == sorted(letter_collection)
+    True
+    >>> import random
+    >>> random_collection = random.sample(range(-50, 50), 100)
+    >>> bubble_sort(random_collection) == sorted(random_collection)
+    True
+    >>> student_tuples = [
+    ...     ('bob', '1', 17),
+    ...     ('carroll', '3', 19),
+    ...     ('alice', '2', 18),
+    ... ]
+    >>> bubble_sort(student_tuples)
+    [('alice', '2', 18), ('bob', '1', 17), ('carroll', '3', 19)]
+    """
+
+    collection: list[TypeItems] = list(iterable)
+    length = len(collection)
+    for i in range(length - 1):
+        is_swapped = False
+        for j in range(length - 1 - i):
+            if collection[j] > collection[j + 1]:
+                collection[j], collection[j + 1] = collection[j + 1], collection[j]
+                is_swapped = True
+        if not is_swapped:
+            break  # Stop iteration if the collection is sorted.
+    return collection
+
+
+if __name__ == '__main__':
+    user_input = input('Enter numbers separated by a comma:').strip()
+    unsorted = [int(item) for item in user_input.split(',')]
+    print(*bubble_sort(unsorted), sep=',')
---
+++ algorithms/sorts/bubble_sort_test.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Testing Bubble Sort"""
+
+from bubble_sort import bubble_sort
+from pytest import mark
+
+
+@mark.parametrize(
+    'collection, expected',
+    [
+        ([4, 7, 2, 11, 1, 8, 0], [0, 1, 2, 4, 7, 8, 11]),
+        ((4, 7, 2, 11, 1, 8, 0), [0, 1, 2, 4, 7, 8, 11]),
+        ({4, 7, 2, 11, 1, 8, 0}, [0, 1, 2, 4, 7, 8, 11]),
+        ({'a', 'y', 'k', 'f', 'x'}, ['a', 'f', 'k', 'x', 'y']),
+        (list('hello'), ['e', 'h', 'l', 'l', 'o']),
+        (
+            {'one': 1, 'three': 3, 'two': 2, 'five': 5, 'four': 4},
+            ['five', 'four', 'one', 'three', 'two'],
+        ),
+        (
+            [('bob', '1', 17), ('carroll', '3', 19), ('alice', '2', 18)],
+            [('alice', '2', 18), ('bob', '1', 17), ('carroll', '3', 19)],
+        ),
+    ],
+)
+def test_bubble_sort(collection, expected):
+    """Testing bubble_sort"""
+    got = bubble_sort(collection)
+    assert got == expected
