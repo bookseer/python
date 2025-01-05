5f08c5b61b5995083d97b179dcdfa57b6f8cbb6c
askras <askrasnikov@gmail.com>
2025-01-05 16:17:05 +0300

    Изменено: Обновлены зависимости проекта

--- tests/venv_test.py
+++ tests/venv_test.py
@@ -9,4 +9,4 @@ def test_python_version():
     """Checking the python version"""
     major, minor = platform.python_version().split(".")[:2]
     pyversion = f"{major}.{minor}"
-    assert pyversion == "3.12", "Incorrect python version"
+    assert float(pyversion) >= 3.12, "Incorrect python version"
